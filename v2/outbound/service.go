06b53ad26a258286fa80c752ca324a3d424b1afb
vaziria <manorder123@gmail.com>
2025-12-13 15:30:11 +0700

    outboud unimplemented

--- v2/outbound/service.go
+++ v2/outbound/service.go
@@ -1,6 +1,10 @@
 package outbound
 
 import (
+	"context"
+
+	"connectrpc.com/connect"
+	"github.com/pdcgo/schema/services/warehouse_iface/v1"
 	"github.com/pdcgo/shared/interfaces/authorization_iface"
 	"gorm.io/gorm"
 )
@@ -8,6 +12,11 @@ import (
 type outboundImpl struct {
 	db   *gorm.DB
 	auth authorization_iface.Authorization
+}
+
+// OrderDetailSearch implements warehouse_ifaceconnect.OutboundServiceHandler.
+func (o *outboundImpl) OrderDetailSearch(context.Context, *connect.Request[warehouse_iface.OrderDetailSearchRequest]) (*connect.Response[warehouse_iface.OrderDetailSearchResponse], error) {
+	panic("unimplemented")
 }
 
 func NewOutboundService(
