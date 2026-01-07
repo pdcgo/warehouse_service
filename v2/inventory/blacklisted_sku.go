86b98ba6862ce120a1e2794f9d17d3d42c36230a
hfrada <madefrada@gmail.com>
2026-01-07 10:12:44 +0700

    public blacklisted sku

--- v2/inventory/blacklisted_sku.go
+++ v2/inventory/blacklisted_sku.go
@@ -2,22 +2,15 @@ package inventory
 
 import (
 	"context"
-	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/pdcgo/schema/services/warehouse_iface/v1"
-	"github.com/pdcgo/shared/custom_connect"
 	"github.com/pdcgo/shared/db_models"
 )
 
 // Placements implements warehouse_ifaceconnect.InventoryServiceHandler.
 func (i *inventoryServiceImpl) BlacklistedSku(ctx context.Context, req *connect.Request[warehouse_iface.BlacklistedSkuRequest]) (*connect.Response[warehouse_iface.BlacklistedSkuResponse], error) {
 	var err error
-	source, err := custom_connect.GetRequestSource(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	identity := i.
 		auth.
 		AuthIdentityFromHeader(req.Header())
@@ -25,10 +18,6 @@ func (i *inventoryServiceImpl) BlacklistedSku(ctx context.Context, req *connect.
 	err = identity.Err()
 	if err != nil {
 		return nil, err
-	}
-
-	if source.TeamId != 1 {
-		return nil, fmt.Errorf("no permission")
 	}
 
 	db := i.db.WithContext(ctx)
