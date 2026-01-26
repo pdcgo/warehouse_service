dec29dcc3729799bc42b5f7a5579ef5f675bb4e4
vaziria <manorder123@gmail.com>
2026-01-26 13:03:12 +0700

    improve performance

--- v2/inventory/placements_ids.go
+++ v2/inventory/placements_ids.go
@@ -3,14 +3,17 @@ package inventory
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/pdcgo/schema/services/access_iface/v1"
 	"github.com/pdcgo/schema/services/warehouse_iface/v1"
 	"github.com/pdcgo/shared/authorization"
 	"github.com/pdcgo/shared/custom_connect"
+	"github.com/pdcgo/shared/db_connect"
 	"github.com/pdcgo/shared/db_models"
 	"github.com/pdcgo/shared/interfaces/authorization_iface"
+	"gorm.io/gorm"
 )
 
 // PlacementsIDs implements warehouse_ifaceconnect.InventoryServiceHandler.
@@ -75,16 +78,211 @@ func (i *inventoryServiceImpl) PlacementsIDs(
 		}
 	}
 
-	for _, txID := range pay.TxIds {
-		result.BulkPlacements[txID] = &warehouse_iface.BulkPlacement{
-			Data: map[string]*warehouse_iface.PlacementDetail{},
-		}
-		result.BulkPlacements[txID].Data, err = i.getPlacements(db, txID)
+	// getting placement bulk
+	invHistMap := map[uint64][]*db_models.InvertoryHistory{}
+	rackMap := map[uint64]*db_models.Rack{}
+	variantMap := map[uint64]*db_models.VariationValue{}
+	skuMap := map[string]*db_models.Sku{}
+	variantIDs := []uint{}
+	rackIDs := []uint{}
+	skuIDs := []string{}
+
+	_, err = db_connect.NewQueryChain(db,
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // preloading histories
+			return func(query *gorm.DB) (*gorm.DB, error) {
+
+				hists := []*db_models.InvertoryHistory{}
+				err = db.
+					Model(&db_models.InvertoryHistory{}).
+					Where("tx_id in ?", pay.TxIds).
+					Find(&hists).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, hist := range hists {
+					txId := uint64(*hist.TxID)
+					if invHistMap[txId] == nil {
+						invHistMap[txId] = []*db_models.InvertoryHistory{}
+					}
+					invHistMap[txId] = append(invHistMap[txId], hist)
+					skuData, err := hist.SkuID.Extract()
+					if err != nil {
+						return nil, err
+					}
+					variantIDs = append(variantIDs, uint(skuData.VariantID))
+					rackIDs = append(rackIDs, hist.RackID)
+					skuIDs = append(skuIDs, string(hist.SkuID))
+				}
+
+				return next(query)
+			}
+		},
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // getting sku
+			return func(query *gorm.DB) (*gorm.DB, error) {
+				if len(skuIDs) == 0 {
+					return next(query)
+				}
+
+				skuIDs = Unique(skuIDs)
+
+				skus := []*db_models.Sku{}
+				err = db.
+					Model(&db_models.Sku{}).
+					Where("id in ?", skuIDs).
+					Find(&skus).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, sku := range skus {
+					skuMap[string(sku.ID)] = sku
+				}
+
+				return next(query)
+			}
+		},
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // getting rack
+			return func(query *gorm.DB) (*gorm.DB, error) {
+				if len(rackIDs) == 0 {
+					return next(query)
+				}
+
+				rackIDs = Unique(rackIDs)
+
+				racks := []*db_models.Rack{}
+				err = db.
+					Model(&db_models.Rack{}).
+					Where("id in ?", rackIDs).
+					Find(&racks).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, rack := range racks {
+					rackMap[uint64(rack.ID)] = rack
+				}
+
+				return next(query)
+			}
+		},
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // getting variant
+			return func(query *gorm.DB) (*gorm.DB, error) {
+				if len(variantIDs) == 0 {
+					return next(query)
+				}
+
+				variantIDs = Unique(variantIDs)
+
+				variants := []*db_models.VariationValue{}
+
+				err = db.
+					Model(&db_models.VariationValue{}).
+					Preload("Product").
+					Where("id in ?", variantIDs).
+					Find(&variants).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, variant := range variants {
+					variantMap[uint64(variant.ID)] = variant
+				}
+
+				return next(query)
+			}
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// add to result
+	for txID, hists := range invHistMap {
+		result.BulkPlacements[txID] = &warehouse_iface.BulkPlacement{}
+		result.BulkPlacements[txID].Data, err = histToPlacementDetail(rackMap, skuMap, variantMap, hists)
 
 		if err != nil {
 			return nil, err
 		}
 	}
 
+	// old ---------------------------
+
 	return connect.NewResponse(&result), nil
+}
+
+func Unique[A comparable](input []A) []A {
+	seen := make(map[A]bool) // A map to track seen elements.
+	var result []A           // A slice to store the unique elements.
+
+	for _, v := range input {
+		if !seen[v] {
+			seen[v] = true
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func histToPlacementDetail(
+	rackMap map[uint64]*db_models.Rack,
+	skuMap map[string]*db_models.Sku,
+	variantMap map[uint64]*db_models.VariationValue,
+	hists []*db_models.InvertoryHistory) (map[string]*warehouse_iface.PlacementDetail, error) {
+	res := map[string]*warehouse_iface.PlacementDetail{}
+
+	for _, hist := range hists {
+		if res[string(hist.SkuID)] != nil {
+			continue
+		}
+		sku := skuMap[string(hist.SkuID)]
+		if sku == nil {
+			return nil, fmt.Errorf("sku empty %s", hist.SkuID)
+
+		}
+
+		variant := variantMap[uint64(sku.VariantID)]
+		if variant == nil {
+			return nil, fmt.Errorf("variant empty %d", sku.VariantID)
+		}
+
+		res[string(hist.SkuID)] = &warehouse_iface.PlacementDetail{
+			Racks: []*warehouse_iface.RackPlacement{},
+			SkuDetail: &warehouse_iface.SkuDataDetail{
+				ProductId:   uint64(sku.VariantID),
+				VariantId:   uint64(sku.VariantID),
+				WarehouseId: uint64(sku.WarehouseID),
+				TeamId:      uint64(sku.TeamID),
+			},
+			VariantDetail: &warehouse_iface.PlacementVariantDetail{
+				Id:           uint64(sku.VariantID),
+				Name:         variant.Product.Name,
+				Image:        variant.Product.Image[0],
+				VariantRefId: string(variant.RefID),
+			},
+		}
+	}
+
+	for _, hist := range hists {
+		rack := rackMap[uint64(hist.RackID)]
+		if rack == nil {
+			return nil, fmt.Errorf("rack empty %d", hist.RackID)
+		}
+		res[string(hist.SkuID)].Racks = append(res[string(hist.SkuID)].Racks, &warehouse_iface.RackPlacement{
+			RackId:    uint64(hist.RackID),
+			ItemCount: int64(hist.Count),
+			RackName:  rack.Name,
+		})
+	}
+
+	return res, nil
 }
--- v2/outbound/outbound_detail_ids.go
+++ v2/outbound/outbound_detail_ids.go
@@ -3,14 +3,18 @@ package outbound
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/pdcgo/schema/services/access_iface/v1"
 	"github.com/pdcgo/schema/services/warehouse_iface/v1"
 	"github.com/pdcgo/shared/authorization"
 	"github.com/pdcgo/shared/custom_connect"
+	"github.com/pdcgo/shared/db_connect"
 	"github.com/pdcgo/shared/db_models"
 	"github.com/pdcgo/shared/interfaces/authorization_iface"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"gorm.io/gorm"
 )
 
 // OutboundDetailIDs implements warehouse_ifaceconnect.OutboundServiceHandler.
@@ -71,15 +75,171 @@ func (o *outboundImpl) OutboundDetailIDs(
 		}
 	}
 
+	// mapping data
+	orderTxIds := []uint64{}
+	txMap := map[uint64]*db_models.InvTransaction{}
+	orderMap := map[uint64]*db_models.Order{}
+
+	_, err = db_connect.NewQueryChain(db,
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // getting transaction
+			return func(query *gorm.DB) (*gorm.DB, error) {
+				trans := []*db_models.InvTransaction{}
+				qinv := db.
+					Model(&db_models.InvTransaction{})
+
+				if pay.LoadAll {
+					qinv = qinv.
+						Preload("Items")
+				}
+
+				err = qinv.
+					Where("id in ?", pay.TxIds).
+					Find(&trans).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, tran := range trans {
+					txMap[uint64(tran.ID)] = tran
+					switch tran.Type {
+					case db_models.InvTxOrder:
+						orderTxIds = append(orderTxIds, uint64(tran.ID))
+					default:
+						return nil, fmt.Errorf("outbound type %s not supported", tran.Type)
+					}
+				}
+
+				return next(query)
+			}
+
+		},
+		func(db *gorm.DB, next db_connect.NextFunc) db_connect.NextFunc { // getting order
+			return func(query *gorm.DB) (*gorm.DB, error) {
+				if !pay.LoadAll {
+					return next(query)
+				}
+
+				if len(orderTxIds) == 0 {
+					return next(query)
+				}
+
+				orders := []*db_models.Order{}
+
+				err = db.
+					Model(&db_models.Order{}).
+					Where("invertory_tx_id in ?", orderTxIds).
+					Find(&orders).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				for _, ord := range orders {
+					orderMap[uint64(*ord.InvertoryTxID)] = ord
+				}
+
+				return next(query)
+			}
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// adding to result
 	result := warehouse_iface.OutboundDetailIDsResponse{
 		Data: map[uint64]*warehouse_iface.OutboundDetailResponse{},
 	}
 
-	for _, txId := range pay.TxIds {
-		result.Data[txId], err = o.outboundDetailItem(db, txId, pay.LoadAll)
-		if err != nil {
-			return nil, err
+	for _, tran := range txMap {
+
+		outbound := txMap[uint64(tran.ID)]
+		outitems := make([]*warehouse_iface.OutboundItem, len(outbound.Items))
+		for i, item := range outbound.Items {
+			skudata, err := item.SkuID.Extract()
+			if err != nil {
+				return nil, err
+			}
+
+			outitems[i] = &warehouse_iface.OutboundItem{
+				Id:    uint64(item.ID),
+				SkuId: string(item.SkuID),
+				Count: int64(item.Count),
+				Owned: item.Owned,
+				Total: item.Total,
+				Price: item.Price,
+				SkuDetail: &warehouse_iface.SkuDataDetail{
+					ProductId:   uint64(skudata.ProductID),
+					VariantId:   uint64(skudata.VariantID),
+					WarehouseId: uint64(skudata.WarehouseID),
+					TeamId:      uint64(skudata.TeamID),
+				},
+			}
 		}
+
+		var shippingID uint64
+		if outbound.ShippingID != nil {
+			shippingID = uint64(*outbound.ShippingID)
+		}
+
+		result.Data[uint64(tran.ID)] = &warehouse_iface.OutboundDetailResponse{
+			Outbound: &warehouse_iface.Outbound{
+				Id:          uint64(outbound.ID),
+				TeamId:      uint64(outbound.TeamID),
+				WarehouseId: uint64(outbound.WarehouseID),
+				CreateById:  uint64(outbound.CreateByID),
+				ExternOrdId: outbound.ExternOrdID,
+				Status:      string(outbound.Status),
+				Receipt:     outbound.Receipt,
+				ReceiptFile: outbound.ReceiptFile,
+				IsDeleted:   outbound.Deleted,
+				IsShipped:   outbound.IsShipped,
+				Created:     timestamppb.New(outbound.Created),
+				Items:       outitems,
+				ShippingId:  shippingID,
+			},
+		}
+
+		if !pay.LoadAll {
+			continue
+		}
+
+		// getting extra
+		res := result.Data[uint64(tran.ID)]
+		switch outbound.Type {
+		case db_models.InvTxOrder:
+			ord := orderMap[uint64(outbound.ID)]
+
+			orddetail := warehouse_iface.OrderDetail{
+				Id:          uint64(ord.ID),
+				TeamId:      uint64(ord.TeamID),
+				CreateById:  uint64(ord.CreatedByID),
+				DoubleOrder: ord.DoubleOrder,
+				OrderRefId:  ord.OrderRefID,
+				OrderFrom:   string(ord.OrderFrom),
+				OrderTime:   timestamppb.New(ord.OrderTime),
+			}
+
+			if ord.InvertoryTxID != nil {
+				orddetail.InvertoryTxId = uint64(*ord.InvertoryTxID)
+			}
+
+			if ord.InvertoryReturnTxID != nil {
+				orddetail.InvertoryReturnTxId = uint64(*ord.InvertoryReturnTxID)
+			}
+
+			res.Extra = &warehouse_iface.OutboundDetailResponse_OrderDetail{
+				OrderDetail: &orddetail,
+			}
+
+		default:
+			return nil, fmt.Errorf("outbound type %s not implemented", outbound.Type)
+		}
+
 	}
 
 	return connect.NewResponse(&result), nil
