5bdea522b8debf5f4d86872b435863ff8095ec01
vaziria <manorder123@gmail.com>
2026-04-10 15:04:06 +0700

    detail seearch

---
+++ v2/outbound/order_detail_search.go
@@ -0,0 +1,211 @@
+package outbound
+
+import (
+	"context"
+	"strings"
+
+	"connectrpc.com/connect"
+	"github.com/pdcgo/schema/services/warehouse_iface/v1"
+	"github.com/pdcgo/shared/db_models"
+	"github.com/pdcgo/shared/pkg/common_helper"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"gorm.io/gorm"
+)
+
+// OrderDetailSearch implements warehouse_ifaceconnect.OutboundServiceHandler.
+func (o *outboundImpl) OrderDetailSearch(
+	ctx context.Context,
+	req *connect.Request[warehouse_iface.OrderDetailSearchRequest],
+) (*connect.Response[warehouse_iface.OrderDetailSearchResponse], error) {
+
+	var err error
+
+	txmap := map[uint64]*warehouse_iface.TransactionDetail{}
+	retTxMap := map[uint64]*warehouse_iface.TransactionDetail{}
+
+	pay := req.Msg
+	result := &warehouse_iface.OrderDetailSearchResponse{}
+
+	db := o.db.WithContext(ctx).Debug()
+
+	caller := common_helper.NewChainParam(
+		func(next common_helper.NextFuncParam[*gorm.DB]) common_helper.NextFuncParam[*gorm.DB] {
+			return func(query *gorm.DB) (*gorm.DB, error) { // creating base query
+
+				orderQuery := query.
+					Table("public.orders o")
+
+				switch search := pay.Search.(type) {
+				case *warehouse_iface.OrderDetailSearchRequest_Receipt: // receipt
+					receipt := strings.ToLower(search.Receipt)
+					receipt = strings.TrimSpace(receipt) + "%"
+					txQuery := query.
+						Table("public.inv_transactions it").
+						Where("it.receipt ilike ?", receipt).
+						Where("it.id = o.invertory_tx_id or it.id = o.invertory_return_tx_id").
+						Select("1")
+
+					orderQuery = orderQuery.
+						Where("EXISTS (?)", txQuery)
+
+				case *warehouse_iface.OrderDetailSearchRequest_OrderRefId: // order_ref_id
+					orderRefId := strings.TrimSpace(search.OrderRefId) + "%"
+					orderQuery = orderQuery.
+						Where("o.order_ref_id ilike ?", orderRefId)
+				case *warehouse_iface.OrderDetailSearchRequest_Customer: // customer
+					name := strings.TrimSpace(search.Customer.Name)
+					city := strings.TrimSpace(search.Customer.City)
+					custQuery := query.
+						Table("public.customer_addresses ca")
+
+					if name != "" {
+						custQuery = custQuery.Where("ca.name ilike ?", name+"%")
+					}
+					if city != "" {
+						custQuery = custQuery.Where("ca.city ilike ?", city+"%")
+					}
+					custQuery = custQuery.Where("o.id = ca.order_id").
+						Select("1")
+
+					orderQuery = orderQuery.
+						Where("EXISTS (?)", custQuery)
+
+				}
+
+				if pay.Status != "" {
+					orderQuery = orderQuery.
+						Where("o.status = ?", pay.Status)
+				}
+
+				return next(orderQuery)
+			}
+		},
+		func(next common_helper.NextFuncParam[*gorm.DB]) common_helper.NextFuncParam[*gorm.DB] {
+			return func(query *gorm.DB) (*gorm.DB, error) { // loading orders
+				var err error
+
+				orders := []*db_models.Order{}
+				err = query.
+					Limit(int(pay.Limit)).
+					Find(&orders).
+					Error
+
+				if err != nil {
+					return nil, err
+				}
+
+				result.Data = make([]*warehouse_iface.OrderDetailSearchItem, len(orders))
+
+				for i, ord := range orders {
+					var invertoryTxId uint64
+					if ord.InvertoryTxID != nil {
+						invertoryTxId = uint64(*ord.InvertoryTxID)
+						txmap[invertoryTxId] = &warehouse_iface.TransactionDetail{}
+					}
+
+					var invertoryReturnTxId uint64
+					if ord.InvertoryReturnTxID != nil {
+						invertoryReturnTxId = uint64(*ord.InvertoryReturnTxID)
+						retTxMap[invertoryReturnTxId] = &warehouse_iface.TransactionDetail{}
+					}
+
+					result.Data[i] = &warehouse_iface.OrderDetailSearchItem{
+
+						Order: &warehouse_iface.OrderDetailSearch{
+							Id:                  uint64(ord.ID),
+							TeamId:              uint64(ord.TeamID),
+							CreateById:          uint64(ord.CreatedByID),
+							InvertoryTxId:       invertoryTxId,
+							InvertoryReturnTxId: invertoryReturnTxId,
+							Status:              string(ord.Status),
+							DoubleOrder:         ord.DoubleOrder,
+							OrderRefId:          ord.OrderRefID,
+							Receipt:             ord.Receipt,
+							ReturnReceipt:       ord.ReceiptReturn,
+							OrderFrom:           ord.OrderFrom.ToProto(),
+							OrderTime:           timestamppb.New(ord.OrderTime),
+						},
+						Outbound: txmap[invertoryTxId],
+						Inbound:  retTxMap[invertoryReturnTxId],
+					}
+				}
+
+				return next(query)
+			}
+		},
+
+		PreloadTransaction(db, txmap),
+		PreloadTransaction(db, retTxMap),
+	)
+
+	_, err = caller(db)
+
+	return connect.NewResponse(result), err
+}
+
+func PreloadTransaction(db *gorm.DB, txmap map[uint64]*warehouse_iface.TransactionDetail) common_helper.NextHandlerParam[*gorm.DB] {
+	return func(next common_helper.NextFuncParam[*gorm.DB]) common_helper.NextFuncParam[*gorm.DB] {
+		return func(query *gorm.DB) (*gorm.DB, error) {
+			var err error
+
+			if len(txmap) == 0 {
+				return next(query)
+			}
+
+			txIds := make([]uint64, 0, len(txmap))
+			for txId := range txmap {
+				txIds = append(txIds, txId)
+			}
+
+			txs := []*db_models.InvTransaction{}
+			err = db.
+				Model(&db_models.InvTransaction{}).
+				Where("id in ?", txIds).
+				Find(&txs).
+				Error
+
+			if err != nil {
+				return nil, err
+			}
+
+			for _, tx := range txs {
+				data := &warehouse_iface.TransactionDetail{
+					Id:          uint64(tx.ID),
+					TeamId:      uint64(tx.TeamID),
+					WarehouseId: uint64(tx.WarehouseID),
+					CreateById:  uint64(tx.CreateByID),
+					ExternOrdId: tx.ExternOrdID,
+					Receipt:     tx.Receipt,
+					ReceiptFile: tx.ReceiptFile,
+					// Type:        tx.Type,
+					// Status:      tx.Status,
+					IsShipped: tx.IsShipped,
+					Total:     tx.Total,
+					Created:   timestamppb.New(tx.Created),
+				}
+
+				if tx.VerifyByID != nil {
+					data.VerifyById = uint64(*tx.VerifyByID)
+				}
+
+				if tx.ShippingID != nil {
+					data.ShippingId = uint64(*tx.ShippingID)
+				}
+
+				if tx.SendAt != nil {
+					data.SendAt = timestamppb.New(*tx.SendAt)
+				}
+
+				if tx.Arrived != nil {
+					data.Arrived = timestamppb.New(*tx.Arrived)
+				}
+
+				proto.Merge(txmap[uint64(tx.ID)], data)
+			}
+
+			return next(query)
+
+		}
+	}
+}
--- v2/outbound/service.go
+++ v2/outbound/service.go
@@ -1,10 +1,6 @@
 package outbound
 
 import (
-	"context"
-
-	"connectrpc.com/connect"
-	"github.com/pdcgo/schema/services/warehouse_iface/v1"
 	"github.com/pdcgo/shared/interfaces/authorization_iface"
 	"gorm.io/gorm"
 )
@@ -12,11 +8,6 @@ import (
 type outboundImpl struct {
 	db   *gorm.DB
 	auth authorization_iface.Authorization
-}
-
-// OrderDetailSearch implements warehouse_ifaceconnect.OutboundServiceHandler.
-func (o *outboundImpl) OrderDetailSearch(context.Context, *connect.Request[warehouse_iface.OrderDetailSearchRequest]) (*connect.Response[warehouse_iface.OrderDetailSearchResponse], error) {
-	panic("unimplemented")
 }
 
 func NewOutboundService(
